378e47e488e9cbc105c22e03a2cb6238a4db7686
isaacBats <disa.indus@gmail.com>
2013-08-01 19:09:09 -0500

    mi primer envio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<link rel="shortcut icon"  href="/images/icono/farico.ico">
+		<title>Farmacias "El Fenix del Centro" S.A. de C.V.</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width = device-width, initial-scale=1, maximum-scale=1"/>
+		<link href="/css/styleIndex.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="main">
+			<header id="cabecera">
+				<div id="cabeza">
+					<h1 class="logo-grande">
+						<a href="/" title="fenixreportes.homelunix.com">
+							<img src="/images/logos/Fenix448x207.png" alt="fenixreportes.homelinux.com" height="124" width="269" />
+						</a>
+					</h1>
+				</div>
+			</header>
+			<section id="entorno">
+			</section>
+			<footer id="pie">
+			</footer>
+		</div>
+	</body>
+</html>
