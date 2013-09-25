486f5350c120906fa4194bdc530dfe923b7f3e0f
isaacBats <disa.indus@gmail.com>
2013-09-24 21:47:03 -0500

    Solo faltan las pag de sucursales

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!--
+Esta Pagina fue elaborada por:	Daniel Bautista klonate@yahoo.com.mx
+Para uso de: Fenix Farmacias en general y Clientes
+el menu que se muestra en esta pag fue hecho con el codigo de la sig pag. : http://www.red-team-design.com/wp-content/uploads/2011/11/css3-animated-dropdown-menu-demo.html
+y los efectos de fooder fueron tomados de esta otra: http://zurb.com/playground/reveal-modal-plugin
+Copyright 2013, Isaac Daniel Batista ó Daniel Bautista.
+Se autoriza copiar el codigo siempre y cuando reconoscan el trabajo de un servidor y le den el credito.
+-->
+
 <!DOCTYPE html>
 <html lang="es">
 	<head>
@@ -42,12 +51,6 @@
 									<li><a href="Zcentro">Zona Centro</a></li>
 								</ul>
 							</li>
-							<!--<li><a href="">Ayuda</a>
-								<ul>
-									<li><a href="Contacto">Contacto</a></li>
-									<li><a href="AvisoPriv">Aviso de Privacidad</a></li>
-								</ul>
-							</li>-->
 							<li id="entrada"><a href="IniSesion">Iniciar Sesion</a></li>
 						</ul>
 					</nav>
@@ -81,7 +84,6 @@
 					<img class="img-slider 3" src="images/bodys/muestra02.jpg" alt="Descuento El Fénix del Centro" height="380" width="100%"/>
 					<img class="img-slider 4" src="images/bodys/muestra03.jpg" alt="Descuento El Fénix del Centro" height="380" width="100%"/>
 					<img class="img-slider 5" src="images/bodys/muestra04.jpg" alt="Descuento El Fénix del Centro" height="380" width="100%"/>            
-        			<!--<img class="img-slider" src="images/bodys/portada.jpg" alt="Farmacias El Fénix del Centro" height="380" width="600"/>-->
 				</div>
 <script src="/js/sliderIndex.js" type="text/javascript"></script>   
 	<script>
@@ -99,62 +101,8 @@
 			$("#slider-imag img").eq(i).fadeIn("medium");
 		}
 	</script>
-	<!--
-				<div id="avisos"> <--style="visibility:hidden;">--
-					<div id="aviso" class:"reveal-modal">
-						<p>
-							<h2>Aviso de Privacidad</h2>
-							FARMACIAS EL FENIX DEL CENTRO, S.A. DE C.V. O FARMACIAS EL FENIX, con domicilio en Lago Trasimeno 36, colonia Anáhuac, Delegación Miguel Hidalgo, 
-							México, D.F., C.P. 11360, es responsable de recabar sus datos personales, del uso que se le dé a los mismos y de su protección. 
-							Su información personal y de la empresa serán utilizada para proveer los servicios y productos que ha solicitado, informarle sobre cambios 
-							en los mismos y evaluar la calidad del servicio que le brindamos. Para las finalidades antes mencionadas, requerimos obtener los siguientes 
-							datos personales: Nombre, Domicilio, Teléfono, Correo electrónico. 
-							Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar 
-							el consentimiento que para tal fin nos haya otorgado, a través de los procedimientos que hemos implementado. 
-							Para conocer dichos procedimientos, los requisitos y plazos, se puede poner en contacto con nuestro departamento de datos personales en la dirección
-							ya mencionada ó ponerse en contacto al conmutador: 91400700 exts. 106, 198, datospersonales@farfenix.com.mx. 
-							Asimismo, le informamos que sus datos personales pueden ser transferidos y tratados dentro y fuera del país, por personas distintas a esta empresa. 
-							En ese sentido, su información puede ser compartida con [señalar el tipo de destinatarios de estas transferencias], para [describir finalidades]. 
-							Si usted no manifiesta su oposición para que sus datos personales sean transferidos, se entenderá que ha otorgado su consentimiento para ello. 
-							No consiento que mis datos personales sean transferidos en los términos que señala el presente aviso de privacidad. 
-							Si usted desea dejar de recibir mensajes promocionales de nuestra parte puede solicitarlo a través de [teléfono, dirección, correo electrónico]. 
-							Cualquier modificación a este aviso de privacidad podrá consultarla en nuestra página web www.farfenix.com.mx Fecha última actualización 06/06/2011.
-						</p>
-						<a class="close-reveal-modal">&#215;</a>
-					</div>
-					<div id="usuario" class:"reveal-modal">
-						<p>
-							<h2>Condiciones de utilización para el usuario</h2>
-							Los usuarios pueden tener acceso y ver el contenido de este sitio web gratuitamente, conforme a estos términos y a cualquier 
-							término específico descrito en cada contenido, producto o servicio. Para recibir ciertos servicios, el acceso a ciertas áreas del sitio web puede 
-							requerir completar un formulario específico de datos - los campos marcados con un asterisco indican que es obligatorio cumplimentar los datos. 
-							Si esos datos no se proporcionan, la empresa se reserva el derecho de no proporcionar el servicio. El usuario garantiza que cualquier dato que 
-							proporcione es completo y exacto, y será responsable de cualesquiera datos falsos o inexactos. Los datos personales serán procesados de acuerdo 
-							con nuestra Política de privacidad. El usuario se compromete a hacer un uso adecuado, correcto y lícito de los contenidos y servicios del sitio web. 
-							Quedan estrictamente prohibidas las actividades ilegales, ilícitas o contrarias a la buena fe y al orden público; y en general, toda conducta que atente, 
-							induzca o pueda atentar contra el respeto a la dignidad humana y al principio de no discriminación por motivos de raza, sexo, religión, opinión, 
-							nacionalidad o cualquier otra circunstancia personal o social; contra la protección de la salud pública y de los consumidores y usuarios; o la protección 
-							de la juventud y de la infancia. Asimismo queda prohibida toda actividad dirigida a suplantar a cualquier persona o entidad, o a interferir, violar, 
-							alterar o desconectar el sistema, servidores, redes o contenidos, así como, incumplir cualquier requisito de conexión. La Empresa se reserva el 
-							derecho a excluir al usuario del servicio sin previo aviso, y a adoptar cuantas medidas considere oportunas en cada momento, a fin de evitar las 
-							conductas y actividades indicadas.
-						</p>
-						<a class="close-reveal-modal">&#215;</a>
-					</div>
-					<div id="contacto" class:"reveal-modal">
-						<p>
-							<h2>Estamos a sus ordenes por los siguientes medios</h2>
-							
-							Conmutador 91 400 700
-							Lada sin costo 01 800 83 62 132
-							Correo electónico backoffice@farfenix.com.mx
-							Web: www.farfenix.com.mx
-						</p>
-						<a class="close-reveal-modal">&#215;</a>
-					</div> 
-					 <div class="reveal-modal-bg"></div> //comentado anteriormente
-				</div>-->
-			</section>
+	
+		</section>
 			<footer id="pie">
 				<section class="primerfo">
 					<div id="logo_pie">
@@ -231,9 +179,12 @@
 			<div id="contac" class="reveal-modal">
 				<p>
 					<h2>Estamos a sus ordenes por los siguientes medios</h2>
-					Conmutador 91 400 700
-					Lada sin costo 01 800 83 62 132
-					Correo electónico backoffice@farfenix.com.mx
+					Lada sin costo 01 800 83 62 132<br>
+					Conmutador 91 400 700 <br>
+					Ext. 166, 651 &nbsp; &nbsp; &nbsp;&nbsp;Backoffice<br>
+					Ext. 112, 677, 170	&nbsp;Sistemas.<br>
+					Ext. 103 &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;Recursos Humannos<br>
+					Correo electónico: backoffice@farfenix.com.mx<br>
 					Web: www.farfenix.com.mx
 				</p>
 				<a class="close-reveal-modal">&#215;</a>				
